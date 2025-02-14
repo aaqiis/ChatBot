d4d9417f46be4c3484343d5b398b96fbe0e6d88b
Anjang Qismatul <anjangqismatulislam@gmail.com>
2025-02-14 11:09:56 +0700

    first

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+# API key untuk OpenAI (RapidAPI)
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+
+# Endpoint API BMKG untuk prakiraan cuaca
+BMKG_API_ENDPOINT = os.environ["BMKG_API_ENDPOINT"]
+
